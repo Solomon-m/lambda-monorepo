8e058ff97f30d7270f1661cb5df79cdc203e50a1
solomon-m <solomon.mark@anywhere.co>
2023-05-15 12:49:14 +0530

    gh-action log chagnes

--- example/lambda-gh-action/index.js
+++ example/lambda-gh-action/index.js
@@ -1,5 +1,5 @@
 exports.handler = async () => {
-  console.log('lambda-gh-action.');
+  console.log('lambda-gh-action.....test->');
   const response = {
     statusCode: 200,
     body: JSON.stringify(`lambda-gh-action ${new Date()}`),
